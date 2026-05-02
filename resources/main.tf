32fcc7a7a9a780060a4f4c1610c31af687e9105d
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-02 08:50:09 +0400

    test vm created

--- resources/main.tf
+++ resources/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vm_creation" {
   ami           = "ami-076d128fb049922d4"
   instance_type = "t3.small"
-
+  security_group_id = "Terraform-Allow-All"
   tags = {
     Name = "TestMachine"
   }
