86e6dab8fb25bdfd5c8781def507dd4bf8dfb869
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-02 08:55:56 +0400

    test vm created

--- resources/main.tf
+++ resources/main.tf
@@ -1,13 +1,8 @@
 resource "aws_instance" "vm_creation" {
   ami           = "ami-076d128fb049922d4"
   instance_type = "t3.small"
-  vpc_security_group_ids = [aws_vpc_security_group_ingress_rule.allow-firewall-rule.security_group_id]
+  vpc_security_group_ids = ["sg-0fcfe62979bdd0bf1"]
   tags = {
     Name = "TestMachine"
   }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow-firewall-rule" {
-  security_group_id = "sg-0fcfe62979bdd0bf1"  # quotes were missing
-  ip_protocol       = "tcp"
 }
